072ca942d9e58334b4ac49c4a46a83b4e140d014
Xnb <201@me.here>
2022-07-31 12:18:15 +0800

    Bug Fixed

---
+++ src/Test/TreeSetTest.java
@@ -0,0 +1,45 @@
+package Test; 
+
+import java.util.*;
+
+public class TreeSetTest{
+    Integer runCount;
+
+    public static void main(String[] arg){
+        var tree= new TreeSet<>();
+
+        int[] org={33,44,22,66,-23,4,65,34,86,-2,-99,-43,-84};
+
+        for(int n : org){
+            tree.add(n);
+        }
+        System.out.println("all the tree");
+        System.out.println(tree);
+
+        System.out.println("First one is "+tree.first());
+        System.out.println("Last one is "+tree.last());
+
+        Scanner keyin = new Scanner(System.in);
+
+        int in=0;
+
+        while(true){
+            try{
+                System.out.print("Input your number ");
+                in=Integer.parseInt(keyin.nextLine());
+            }catch(NumberFormatException ex){
+                System.out.println("error input , try again");
+                continue;
+            }
+
+            if(in < (int)tree.first() || in > (int)tree.last()){
+                continue;
+            }else{
+                break;
+            }
+        }
+        System.out.print("equal or bigger then "+in+" are:");
+        System.out.println(tree.tailSet(in));
+        keyin.close();
+    }
+}
